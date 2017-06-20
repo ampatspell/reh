979d5e8e18a3eefc647d57ec77114623b8237612
ampatspell <ampatspell@gmail.com>
2017-06-20 17:12:48 +0300

    form ui

--- app/controllers/internal/ui/form.js
+++ app/controllers/internal/ui/form.js
@@ -1,6 +1,22 @@
 import Ember from 'ember';
 
+const {
+  computed
+} = Ember;
+
+const Model = Ember.Object.extend({
+  fistName: null,
+  lastName: null,
+  socialSecurityNumber: null,
+  isReturning: false,
+  diagnosis: null,
+});
+
 export default Ember.Controller.extend({
+
+  model: computed(function() {
+    return Model.create();
+  }).readOnly(),
 
   actions: {
     save() {
